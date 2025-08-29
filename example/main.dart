124919660595909db729ea53552f2c63ff0f2d5e
AjayJasperJ <ajayjasperjames@gmail.com>
2025-08-29 20:26:13 +0530

    example updated

---
+++ example/main.dart
@@ -0,0 +1,91 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_calenders/flutter_calenders.dart';
+
+void main() => runApp(const MyEngine());
+
+class MyEngine extends StatelessWidget {
+  const MyEngine({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(home: MyCalenders());
+  }
+}
+
+class MyCalenders extends StatefulWidget {
+  const MyCalenders({super.key});
+
+  @override
+  State<MyCalenders> createState() => _MyCalendersState();
+}
+
+class _MyCalendersState extends State<MyCalenders> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: SingleChildScrollView(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.spaceAround,
+          crossAxisAlignment: CrossAxisAlignment.center,
+          children: [
+            EventBasedCalender(
+              margin: EdgeInsets.symmetric(horizontal: 10),
+              padding: EdgeInsets.all(10),
+              events: [
+                Event(
+                  eventName: 'Sick leave',
+                  dates: [DateTime(2025, 8, 21), DateTime(2025, 8, 22)],
+                  color: Colors.green,
+                ),
+                Event(
+                  eventName: 'Paid leave',
+                  dates: [DateTime(2025, 8, 17), DateTime(2025, 8, 18)],
+                  color: Colors.orange,
+                ),
+              ],
+              primaryColor: Colors.blue,
+              backgroundColor: Colors.blue.withValues(alpha: .05),
+              chooserColor: Colors.black,
+              endYear: 2028,
+              startYear: 2020,
+              currentMonthDateColor: Colors.black,
+              pastFutureMonthDateColor: Colors.grey,
+              isSelectedColor: Colors.amber,
+              isSelectedShow: true,
+              showEvent: true,
+              onDateTap: (date) {
+                print(date);
+              },
+            ),
+            ScheduleBasedCalender(
+              events: [
+                Event(
+                  eventName: 'Sick leave',
+                  dates: [DateTime(2025, 8, 21), DateTime(2025, 8, 22)],
+                  color: Colors.green,
+                ),
+                Event(
+                  eventName: 'Paid leave',
+                  dates: [DateTime(2025, 8, 17), DateTime(2025, 8, 18)],
+                  color: Colors.orange,
+                ),
+              ],
+              currentMonth: 8,
+              currentYear: 2025,
+              backgroundColor: Colors.white.withValues(alpha: .1),
+              monthDateColor: Colors.white.withValues(alpha: .1),
+              weekdayColor: Colors.white.withValues(alpha: .1),
+              monthTextColor: Colors.white,
+              weekdayTextColor: Colors.white,
+              isSelectedShow: true,
+              isSelectedColor: Colors.deepOrange,
+              onDateTap: (date) {
+                print(date);
+              },
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
